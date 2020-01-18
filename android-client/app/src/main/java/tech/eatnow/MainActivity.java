2324efe60c231d4ebeeb06aba43a6d3e7671eebe
Kyle McLean <kylem590@gmail.com>
2020-01-18 14:17:08 -0700

    The settings menu now exists

--- android-client/app/src/main/java/tech/eatnow/MainActivity.java
+++ android-client/app/src/main/java/tech/eatnow/MainActivity.java
@@ -1,10 +1,12 @@
 package tech.eatnow;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -58,6 +60,20 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return false;
+        }
     }
 
     @Override
